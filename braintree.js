1bfe4e53320b375722916716df5d0a6caae62b10
Gavin Ching <gavinchingy@gmail.com>
2015-09-19 20:40:13 -0400

    Push braintree stuff

--- braintree.js
+++ braintree.js
@@ -7,6 +7,26 @@ var gateway = braintree.connect({
   privateKey: '2d12417b4a738713af464ed689f6b484'
 });
 
-gateway.merchantAccount.create(
-
-);
+gateway.merchantAccount.create({
+  individual: {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    email: 'jane@14ladders.com',
+    phone: '5553334444',
+    dateOfBirth: '1981-11-19',
+    address: {
+      streetAddress: '111 Main St',
+      locality: 'Chicago',
+      region: 'IL',
+      postalCode: '60622'
+    }
+  },
+  funding: {
+    destination: 'email'
+  },
+  tosAccepted: true,
+  masterMerchantAccountId: 'derpina',
+}, function(err, results){
+  console.log(err);
+  console.log(results);
+});
