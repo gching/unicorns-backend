c65582f1675d0b8eb9dd47fe76e8ad6e2980dfdf
Gavin Ching <gavinchingy@gmail.com>
2015-09-19 22:33:54 -0400

    Integrate braintree with users

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,14 @@ var router = express.Router();
 var Firebase = require('firebase');
 var braintree = require('braintree');
 
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: 'zgbd22s3hvs8wz5n',
+  publicKey: '75yxqwfjgyr98nsm',
+  privateKey: 'beb1e6838b2c7a96db149da04521d359'
+});
+
+
 
 // Setup gatway with sandbox.
 // Use Firebase
@@ -46,18 +54,51 @@ router.post('/', function(req, res, next){
       next(error);
     } else {
       console.log("Successfully created user account with uid:", userData.uid);
-      // We also have to create a users on Firebase itself.
+      // Setup the data for the saved user
       var actualUserData = {
         uid: userData.uid,
         email: req.body.email,
-        name: req.body.name || null,
+        first_name: req.body.first_name || null,
+        last_name: req.body.last_name || null,
         phone_number: req.body.phone_number || null,
         credit_card_number: req.body.credit_card_number || null,
         credit_card_expiry_date: req.body.credit_card_expiry_date || null,
         credit_card_cvc: req.body.credit_card_cvc || null
       };
-      usersRef.push(actualUserData);
-      res.send({ user: actualUserData});
+      // Create submerchant on brainTree
+      gateway.merchantAccount.create({
+        individual: {
+          firstName: actualUserData.first_name || 'Alexis',
+          lastName: actualUserData.last_name || 'Ohanian',
+          email: actualUserData.email,
+          phone: actualUserData.phone_number || '6479212199',
+          dateOfBirth: '1981-11-19',
+          address: {
+            streetAddress: '111 Main St',
+            locality: 'Chicago',
+            region: 'IL',
+            postalCode: '60622'
+          }
+        },
+        funding: {
+          destination: 'email',
+          email: actualUserData.email
+        },
+        tosAccepted: true,
+        masterMerchantAccountId: 'randomcompany',
+      }, function(err, results){
+        
+        // Save the submerchant id
+        actualUserData.merchant_id = results.merchantAccount.id;
+
+        // We also have to create a users on Firebase itself.
+        usersRef.push(actualUserData);
+        res.send({ user: actualUserData});
+
+
+      });
+
+
     }
   });
 });
